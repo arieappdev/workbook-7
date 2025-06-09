ccb10f1c777fa010e7717a7c87bab63879c3d96d
Arie Fisher <jbgirl0@gmail.com>
2025-06-09 15:30:11 -0400

    updating pushes for exercises

--- basic-queries/northwind basic queries.sql
+++ basic-queries/northwind basic queries.sql
@@ -21,17 +21,33 @@
 -- with comments before each query and then only run the selected query by
 -- pressing the 2nd lightning bolt.
 -- 1. What is the name of the table that holds the items Northwind sells?
+-- PRODUCTS
 -- 2. Write a query to list the product id, product name, and unit price of every
 -- product.
+USE northwind;
+SELECT ProductID, ProductName, unitprice
+FROM products;
 -- 3. Write a query to list the product id, product name, and unit price of every
 -- product. Except this time, order then in ascending order by price.
+SELECT ProductID, ProductName, UnitPrice
+FROM products
+ORDER BY UnitPrice;
 -- 4. What are the products that we carry where the unit price is $7.50 or less?
 -- 2-34
+SELECT ProductName, UnitPrice
+FROM products
+WHERE UnitPrice <= 7.50;
 -- 5. What are the products that we carry where we have at least 100 units on
 -- hand? Order them in descending order by price.
+SELECT*FROM Products
+WHERE UnitsInStock >= 100
+ORDER BY UnitPrice DESC;
 -- 6. What are the products that we carry where we have at least 100 units on
 -- hand? Order them in descending order by price. If two or more have the
--- same price, list those in ascending order by product name.
+-- same price, list those in ascending order by product name. Greater than or equal to 50
+SELECT*FROM Products
+WHERE UnitsInStock >= 50
+ORDER BY UnitPrice DESC;
 -- 7. What are the products that we carry where we have no units on hand, but 1
 -- or more units of them on backorder? Order them by product name.
 -- 8. What is the name of the table that holds the types (categories) of the items
@@ -41,10 +57,18 @@
 -- 10. Examine the Products table. How does it identify the type (category) of
 -- each item sold? Write a query to list all of the seafood items we carry.
 -- 11. What are the first and last names of all of the Northwind employees?
+SELECT FirstName, LastName
+FROM employees;
 -- 12. What employees have "manager" in their titles?
+SELECT Title
+FROM employees
+WHERE Title LIKE "%manager%";
 -- 13. List the distinct job titles in employees.
+SELECT Title
+FROM employees;
 -- 14. What employees have a salary that is between $200 0 and $2500?
 -- 15. List all of the information about all of Northwind's suppliers.
+SELECT*FROM northwind.suppliers;
 -- 16. Examine the Products table. How do you know what supplier supplies
 -- each product? Write a query to list all of the items that "Tokyo Traders"
 -- supplies to Northwind
---
+++ joins/exercise3.sql
@@ -0,0 +1,32 @@
+-- --1. List the product id, product name, unit price and category name of all
+-- -- products. Order by category name and within that, by product name.
+USE northwind;
+SELECT ProductID, ProductName, UnitPrice, CategoryName
+FROM Products
+INNER JOIN Categories ON Products.CategoryID = Categories.CategoryID
+ORDER BY CategoryName, ProductName;
+
+-- 2. List the product id, product name, unit price and supplier name of all
+-- products that cost more than $75. Order by product name.
+SELECT ProductID, ProductName, UnitPrice, CompanyName
+FROM Products
+INNER JOIN Suppliers ON Products.SupplierID = Suppliers.SupplierID
+WHERE UnitPrice > 75
+ORDER BY ProductName;
+
+-- 3. List the product id, product name, unit price, category name, and supplier
+-- name of every product. Order by product name.
+SELECT ProductID, ProductName, UnitPrice, CategoryName, CompanyName
+FROM Products
+INNER JOIN Suppliers ON Products.SupplierID = Suppliers.SupplierID
+INNER JOIN Categories ON Products.CategoryID = Categories.CategoryID
+ORDER BY ProductName;
+
+-- 4. What is the product name(s) and categories of the most expensive
+-- products? HINT: Find the max price in a subquery and then use that in
+-- your more complex query that joins products with categories.
+-- 5. List the order id, ship name, ship address, and shipping company name of
+-- every order that shipped to Germany.
+-- 6. List the order id, order date, ship name, ship address of all orders that
+-- ordered "Sasquatch Ale"?
+-- Commit and push your code!
